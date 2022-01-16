9cc95a99089aac9ece002fd52d5db16bc88c5f90
Adam <apalucki@poczta.onet.pl>
2022-01-16 17:42:35 +0100

    settings

--- mlib-app/app/urls.py
+++ mlib-app/app/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('book.urls')),
     path('', include('restapi.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
